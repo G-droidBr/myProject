f857ceac7b8af30cbd73e23a54eecae926f35135
John Doe <gustavo.sena0@gmail.comGustavo Barbosa Sena>
2021-06-29 13:25:05 -0300

    Alteraçoes em Algumas Perguntas e CheckWinner

--- index.js
+++ index.js
@@ -1,121 +1,144 @@
 let paper = document.getElementById("paperBack");
 paper.style.display = 'none';
 
+let winnerHtml = document.getElementById("winner");
+winnerHtml.style.display = 'none';
 
 
-let questions =[
+
+let questions = [
     {
-        question:'Qual Time Brasileiro não tem mundial?',
-        answer:`Palmeiras`,
-        option:'Corinthias',
-        option2:'São Paulo',   
-    }
-    // {
-    //     question:'',
-    //     answer:'',
-    //     option:'',
-    //     option2:'',
-    // }
-    // {
-    //     question:'',
-    //     answer:'',
-    //     option:'',
-    //     option2:'',
-    // }
-    // {
-    //     question:'',
-    //     answer:'',
-    //     option:'',
-    //     option2:'',
-    // }
-    // {
-    //     question:'',
-    //     answer:'',
-    //     option:'',
-    //     option2:'',
-    // }
-    // {
-    //     question:'',
-    //     answer:'',
-    //     option:'',
-    //     option2:'',
-    // }
-    // {
-    //     question:'',
-    //     answer:'',
-    //     option:'',
-    //     option2:'',
-    // }
-    // {
-    //     question:'',
-    //     answer:'',
-    //     option:'',
-    //     option2:'',
-    // }
-    // {
-    //     question:'',
-    //     answer:'',
-    //     option:'',
-    //     option2:'',
-    // }
-    // {
-    //     question:'',
-    //     answer:'',
-    //     option:'',
-    //     option2:'',
-    // }
-    // {
-    //     question:'',
-    //     answer:'',
-    //     option:'',
-    //     option2:'',
-    // }
-    // {
-    //     question:'',
-    //     answer:'',
-    //     option:'',
-    //     option2:'',
-    // }
-    // {
-    //     question:'',
-    //     answer:'',
-    //     option:'',
-    //     option2:'',
-    // }
-    // {
-    //     question:'',
-    //     answer:'',
-    //     option:'',
-    //     option2:'',
-    // }
-    // {
-    //     question:'',
-    //     answer:'',
-    //     option:'',
-    //     option2:'',
-    // }
-    
+        question: 'O Estádio do Morumbi pertence a qual time abaixo?',
+        answer: './images/sp.png',
+        option: './images/corinthians.png',
+        option2: './images/palmeirass.png',
+        
+    },
+    {
+        question: 'Qual desses times abaixo é considerado com a maior torcida do brasil?',
+        answer: './images/Flamengo-RJ_(BRA).png',
+        option: './images/sp.png',
+        option2: './images/corinthians.png',
+       
+    },
+    {
+        question: 'Qual deles Atualmente é o time mais rico do brasil?',
+        answer: './images/Flamengo-RJ_(BRA).png',
+        option: './images/palmeirass.png',
+        option2: './images/gremio.png',
+       
+    },
+    {
+        question: 'Qual desses times abaixo é considerado como "Time do Povo"?',
+        answer: './images/corinthians.png',
+        option: './images/Santos.png',
+        option2: './images/ECBahia.png',
+    },
+    {
+        question: 'Qual desses times abaixo é do estado de São Paulo ?',
+        answer: './images/Santos.png',
+        option: './images/Flamengo-RJ_(BRA).png',
+        option2: './images/Atletico_mineiro_galo.png',
+    },
+    {
+        question: 'Qual desses times abaixo é do estado do Rio de Janeiro ?',
+        answer: './images/FluminenseFCBA.png',
+        option: './images/Santos.png',
+        option2: './images/inter.png',
+    },
+    {
+        question: 'Qual clube que teve o maior número de jogadores Campeões Mundiais?',
+        answer: './images/Santos.png',
+        option: './images/Atletico_mineiro_galo.png',
+        option2: './images/corinthians.png',
+    },
+    {
+        question: 'Qual desses times a baixo foi campeão invicto da Libertadores?',
+        answer: './images/corinthians.png',
+        option: './images/Santos.png',
+        option2: './images/inter.png',
+    },
+    {
+        question: 'Qual Foi o Primeiro Clube a Jogar a Taça Libertadores ?',
+        answer: './images/ECBahia.png',
+        option: './images/gremio.png',
+        option2: './images/FluminenseFCBA.png',
+    },
+    {
+        question: 'Qual desses times abaixo participa Campeonato Mineiro ?',
+        answer: './images/Atletico_mineiro_galo.png',
+        option: './images/corinthians.png',
+        option2: './images/gremio.png',
+    },
+    {
+        question: 'Qual desses times abaixo é chamado de São Paulo?',
+        answer: './images/sp.png',
+        option: './images/gremio.png',
+        option2: './images/Santos.png',
+    },
+    {
+        question: 'Qual desses times abaixo é chamado de Santos?',
+        answer: './images/Santos.png',
+        option: './images/sp.png',
+        option2: './images/Atletico_mineiro_galo.png',
+    },
+    {
+        question: 'Qual desses times abaixo é chamado de Palmeiras ?',
+        answer: './images/palmeirass.png',
+        option: './images/gremio.png',
+        option2: './images/ECBahia.png',
+    },
+    {
+        question: 'Qual desses Times abaixo é chamado de Atlético Mineiro?',
+        answer: './images/Atletico_mineiro_galo.png',
+        option: './images/Santos.png',
+        option2: './images/ECBahia.png',
+    },
+    {
+        question: 'Qual desses times abaixo é chamado de Bahia ?',
+        answer: './images/ECBahia.png',
+        option: './images/inter.png',
+        option2: './images/corinthians.png',
+    },
+    {
+        question: 'Qual desses times abaixo é chamado de Gremio?',
+        answer: './images/gremio.png',
+        option: './images/sp.png',
+        option2: './images/Santos.png',
+    },
+    {
+        question: 'Qual desses times abaixo é chamado de Fluminense?',
+        answer: './images/FluminenseFCBA.png',
+        option: './images/sp.png',
+        option2: './images/Flamengo-RJ_(BRA).png',
+    },
+    {
+        question: 'Qual desses times abaixo é chamado de Internacional?',
+        answer: './images/inter.png',
+        option: './images/Atletico_mineiro_galo.png',
+        option2: './images/Flamengo-RJ_(BRA).png',
+    },
+
 ];
 
 
 document.getElementById('startButton').addEventListener('click', function () {
-    // console.log('oiii')
+
     clear();
     startQuestion();
     pushQuestions();
-    
-    
+
+
 });
 
 document.getElementById('')
 
 
 function clear() {
-    
-    document.getElementById("startGame2").style.display = 'none';  
-    
-};
 
+    document.getElementById("startGame2").style.display = 'none';
+
+};
 
 
 
@@ -126,25 +149,144 @@ function startQuestion() {
     
 };
 
-let pergunta = questions[0].question;
-let option = questions[0].option;
-let option2 = questions[0].option2;
-let resposta = questions[0].answer;
+let questionNumber = 0;
+let pergunta = questions[questionNumber].question;
+let option = questions[questionNumber].option;
+let option2 = questions[questionNumber].option2;
+let resposta = questions[questionNumber].answer;
+let help = questions[questionNumber].help;
+function getRandomAnswer() {
 
-function pushQuestions(){
-    
-    
-    document.querySelector('#paperBack h1').innerHTML = `${pergunta}`;
-    document.querySelector('#paperBack ul').innerHTML = `<li>${option}</li>`;
-    document.querySelector('#paperBack ul').innerHTML += `<li>${option2}</li>`;
-    document.querySelector('#paperBack ul').innerHTML += `<li>${resposta}</li>`;
-};
+    let options = [option, option2, resposta]
+    let opcao = Math.floor(Math.random() * 3)
+
+    return options[opcao]
+
+}
+
+let respostasAleatorias = [];
+
+function checkUniqueAnswer() {
+    let answer = getRandomAnswer()
+    if (respostasAleatorias.indexOf(answer) === -1) {
+        respostasAleatorias.push(answer)
+    }
+}
+
+function threeRandomAnswer() {
+
+
+    while (respostasAleatorias.length !== 3) {
+        checkUniqueAnswer()
+    }
+
+}
+
+
 
 let correctAnswers = 0;
+let wrongQuestions = 0;
 
-function nextQuestion(){
+
+
+function pushQuestions() {
+    respostasAleatorias = [];
+    threeRandomAnswer();
+    
+
+    document.querySelector('#paperBack ul').innerHTML = ''
+    document.querySelector('#paperBack h1').innerHTML = `${pergunta}`;
+
+    for(let i = 0;i < respostasAleatorias.length; i+= 1){
+        
+        
+        let id = "error";
+        if(resposta === respostasAleatorias[i]){
+            id = "resposta"
+        }
+        if(option2 === respostasAleatorias[i]){
+            id = "error1"
+        }
+        document.querySelector('#paperBack ul').innerHTML += `<li id = ${id}><img src= ${respostasAleatorias[i]}></li>`
+       
+    }
+
+    
+
+
+    document.querySelector("#resposta").addEventListener('click', function () {
+        correctAnswers += 1;
+        console.log(correctAnswers)
+        nextQuestion();
+
+    })
+    document.querySelector("#error").addEventListener('click', function () {
+        wrongQuestions += 1;
+        nextQuestion();
+        
+    })
+    document.querySelector("#error1").addEventListener('click', function () {
+        wrongQuestions += 1;
+        nextQuestion();
+        
+    })
+   
+    
+    
+
+};
+
+
+
+function nextQuestion() {
+
+    
+    questionNumber += 1;
+
+    if (questionNumber === 10) {
+
+        winner()
+    }
+
+    pergunta = questions[questionNumber].question
+    resposta = questions[questionNumber].answer;
+    option = questions[questionNumber].option;
+    option2 = questions[questionNumber].option2;
+    help = questions[questionNumber].help;
+    pushQuestions()
+ 
+
+}
+
+
+
+function winner() {
+
+    winnerHtml.style.display = 'block';
+    document.querySelector('#clearWinner').innerHTML = '';
+    checkWinner()
     
 }
+
+let porcentagem = 0;
+
+function checkWinner() {
+    porcentagem = correctAnswers * 10
+
+    if (correctAnswers > 6) {
+        document.querySelector('#winner h1').innerHTML = `Parabéns Você Ganhou!`
+        document.querySelector('#winner h3').innerHTML = `${porcentagem}%`
+        document.querySelector('#p1').innerHTML = `Você Acertou ${correctAnswers} Perguntas!`
+        document.querySelector('#p2').innerHTML = `Você Errou ${wrongQuestions} Perguntas!`
+    }
+    else if (correctAnswers < 6) {
+        document.querySelector('#winner h1').innerHTML = `Infelizmente Não Foi Desta Vez!`
+        document.querySelector('#winner h3').innerHTML = `${porcentagem}%`
+        document.querySelector('#p1').innerHTML = `Você Acertou ${correctAnswers} Perguntas!`
+        document.querySelector('#p2').innerHTML = `Você Errou ${wrongQuestions} Perguntas!`
+    }
+}
+
 
 
 
